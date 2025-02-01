48f747004353525d12a92a5c3cb081c71d85f526
QASIM-LOKHANDWALA <qasimlokhandwala1@gmail.com>
2025-02-01 17:48:42 +0530

    Add Nearby Airports Feature

--- airports/urls.py
+++ airports/urls.py
@@ -1,6 +1,7 @@
 from django.urls import path
-from .views import home
+from .views import home,nearby_airports
 
 urlpatterns = [
   path('', home, name='airport_home'),
+  path("nearby/", nearby_airports, name="nearby_airports"),
 ]
--- airports/views.py
+++ airports/views.py
@@ -1,7 +1,41 @@
+import os
+from dotenv import load_dotenv
+import requests
 from django.shortcuts import render
+from django.http import JsonResponse
 from .api_functions import AirportAPI
 
 airport_api = AirportAPI()
+load_dotenv()
+def get_amadeus_token():
+  url = "https://test.api.amadeus.com/v1/security/oauth2/token"
+  data = {
+    "grant_type": "client_credentials",
+    "client_id": os.getenv("CLIENT_ID"),
+    "client_secret": os.getenv("CLIENT_SECRET"),
+  }
+  response = requests.post(url, data=data)
+  return response.json().get("access_token")
+
+def nearby_airports(request):
+  lat = request.GET.get("lat")
+  lon = request.GET.get("lon")
+
+  if not lat or not lon:
+      return JsonResponse({"error": "Latitude and Longitude required"}, status=400)
+
+  token = get_amadeus_token()
+  headers = {"Authorization": f"Bearer {token}"}
+  
+  url = f"https://test.api.amadeus.com/v1/reference-data/locations/airports?latitude={lat}&longitude={lon}&radius=100"
+
+  response = requests.get(url, headers=headers)
+
+  if response.status_code != 200:
+    return JsonResponse({"error": "Failed to fetch airports"}, status=response.status_code)
+
+  return JsonResponse(response.json().get("data", []), safe=False)
+
 
 # Create your views here.
 def home(request):
