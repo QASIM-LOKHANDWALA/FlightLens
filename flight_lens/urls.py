1477837c004d30fa258e5182de6ada2a9c15d722
QASIM-LOKHANDWALA <qasimlokhandwala1@gmail.com>
2025-02-02 15:44:41 +0530

    Create Login Page

--- flight_lens/urls.py
+++ flight_lens/urls.py
@@ -5,6 +5,7 @@ from .views import home
 urlpatterns = [
     path('admin/', admin.site.urls),
     path('', home, name='landing_page'),
+    path('user_auth/',include('user_auth.urls')),
     path('flights/',include('flights.urls')),
     path('airports/',include('airports.urls')),
 ]
--- user_auth/views.py
+++ user_auth/views.py
@@ -1,3 +1,15 @@
 from django.shortcuts import render
 
 # Create your views here.
+def register_view(request):
+  pass
+
+def login_view(request):
+  if request.method == 'POST':
+    email = request.POST.get("email")
+    password = request.POST.get("password")
+    print(email, password)
+  return render(request, 'user_auth/login.html')
+
+def logout_view(request):
+  pass
