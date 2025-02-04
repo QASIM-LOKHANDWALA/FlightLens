477d0ef4c8079dd58f4668e93a089ef68456a80f
QASIM-LOKHANDWALA <qasimlokhandwala1@gmail.com>
2025-02-04 18:12:53 +0530

    Basic User Authentication

--- user_auth/views.py
+++ user_auth/views.py
@@ -1,15 +1,42 @@
-from django.shortcuts import render
+from django.shortcuts import render, redirect
+from django.contrib.auth import login, logout
+from django.contrib.auth.forms import AuthenticationForm
+from django.contrib.auth.forms import UserCreationForm
+from django.contrib.auth.models import User
 
-# Create your views here.
+class CustomUserCreationForm(UserCreationForm):
+  class Meta:
+    model = User
+    fields = ['username', 'password1', 'password2']
+
+
 def register_view(request):
-  return render(request, 'user_auth/register.html')
+  if request.method == 'POST':
+    form = CustomUserCreationForm(request.POST)
+    if form.is_valid():
+      user = form.save()
+      login(request, user)
+      return redirect('dashboard')
+  else:
+    form = CustomUserCreationForm()
+  
+  return render(request, 'user_auth/register.html', {'form': form})
 
 def login_view(request):
   if request.method == 'POST':
-    email = request.POST.get("email")
-    password = request.POST.get("password")
-    print(email, password)
-  return render(request, 'user_auth/login.html')
+    form = AuthenticationForm(request, data=request.POST)
+    if form.is_valid():
+      user = form.get_user()
+      login(request, user)
+      return redirect('dashboard')
+  else:
+    form = AuthenticationForm()
+  
+  return render(request, 'user_auth/login.html', {'form': form})
 
 def logout_view(request):
-  pass
+  logout(request)
+  return redirect('login')
+
+def dashboard_view(request):
+  return render(request, 'user_auth/dashboard.html')
